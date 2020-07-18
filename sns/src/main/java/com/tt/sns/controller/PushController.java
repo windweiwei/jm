8634cacc7fa24620fec56f681fc85ea65b280c85
wei <1013398588@qq.com>
2020-07-18 17:35:55 +0800

    文件补充

--- sns/src/main/java/com/tt/sns/controller/PushController.java
+++ sns/src/main/java/com/tt/sns/controller/PushController.java
@@ -6,6 +6,7 @@ import com.tt.sns.request.PushTokenSaveParam;
 import com.tt.sns.response.BaseResponse;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,11 +23,12 @@ import com.tt.sns.service.PushService;
  */
 @Api(value = "推送模块")
 @RestController
+@Slf4j
 public class PushController {
     @Autowired
     private PushService pushService;
 
-    @ApiOperation(value = "推送token注册接口", notes = "当用户登陆或打开app需要注册推送token",response = BaseResponse.class)
+    @ApiOperation(value = "推送token注册接口", notes = "当用户登陆或打开app需要注册推送token", response = BaseResponse.class)
     @PostMapping("/api/v3/push_token/save")
     public BaseResponse savePushToken(@RequestBody PushTokenSaveParam param, HttpServletRequest request) {
 
@@ -36,6 +38,8 @@ public class PushController {
     @ApiOperation(value = "消息接受并发送接口", notes = "向这个接口发送需要推送当消息")
     @PostMapping("/api/v3/alarm/push")
     public BaseResponse acceptAndPush(@RequestBody Alarm alarm, HttpServletRequest request) {
+        log.info(alarm.toString());
+        log.info(alarm.getUid());
         return pushService.acceptAndPush(alarm, request);
 
     }
@@ -43,6 +47,7 @@ public class PushController {
     @ApiOperation(value = "推送平台相关配置注册", notes = "把平台相关当配置注册到平台")
     @PostMapping("/api/v3/push_config/save")
     public BaseResponse savePushConfig(@RequestBody PushConfig pushConfig, HttpServletRequest request) {
+
         return pushService.savePushConfig(pushConfig, request);
 
     }
