ae4b1e1892337d77a3138ed1bcae3ffef5c3b287
windweiwei <1013398588@qq.com>
2020-08-13 15:20:57 +0800

    Update AliSmsUtil.java
    update

--- common/src/main/java/com/jimang/util/AliSmsUtil.java
+++ common/src/main/java/com/jimang/util/AliSmsUtil.java
@@ -18,7 +18,7 @@ import lombok.extern.slf4j.Slf4j;
  */
 @Slf4j
 public class AliSmsUtil {
-    private static final String accessKeyId = "LTAIGpI4yPje4Ty7";
+    private static final String accessKeyId = "****";
     private static final String accessSecret = "********";
 
     public static Boolean sendSms(String phone, String signName,
